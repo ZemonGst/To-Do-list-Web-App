440f392037e9fc01204f9d4ab9407821a652e03d
Soumyaditya Sinha <sinhasoumyaditya60@gmail.com>
2025-12-20 23:15:49 +0530

    form add commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const addNewTask = document.querySelector('.add-task-btn');
+const formBackdrop = document.querySelector('.backdrop');
+const formContainer = document.querySelector('.container');
+const formClose = document.querySelector('.close-form');
+const formSave = document.querySelector('.Save-form');
+// form inputs
+const titleInput = document.querySelector('.title-input');
+const subtaskInput = document.querySelector('.subtask-input');
+const taskDetails = document.querySelector('.taskDetails');
+const subtaskDetails = document.querySelector('.subtaskDetails');
+const dateInput = document.querySelector('.form-date');
+
+function taskform() {
+    let titleValue = titleInput.value;
+    let subTaskValue = subtaskDetails.value;
+    let taskDetailsValue = taskDetails.value;
+    let subTaskDetailsValue = subtaskDetails.value;
+    let endDate = dateInput.value;
+}
+
+addNewTask.addEventListener('click', () => {
+    formBackdrop.style.display = 'block';
+    formContainer.style.display = 'block';
+
+
+})
+formClose.addEventListener('click', () => {
+    formBackdrop.style.display = 'none';
+    formContainer.style.display = 'none';
+    console.log('working');
+})
+formSave.addEventListener('click', () => {
+    taskform()
+    formBackdrop.style.display = 'none';
+    formContainer.style.display = 'none';
+    console.log('working');
+
+})
+
